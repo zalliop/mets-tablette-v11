e3629f3abf8512991f25fe3379ca547cc621677b
zalliop <88331342+zalliop@users.noreply.github.com>
2026-09-23 21:45:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+/* Permet à l'application de s'ouvrir sans internet une fois installée. */
+const V = "mets-v7";
+const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(V).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then(cached => {
+    const net = fetch(e.request).then(res => {
+      if (res && res.ok) { const c = res.clone(); caches.open(V).then(ca => ca.put(e.request, c)); }
+      return res;
+    }).catch(() => cached);
+    return cached || net;
+  }));
+});
